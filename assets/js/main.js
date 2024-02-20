3c6da89debbe6966c3f05232a9d4a097277b8c01
Khaisen <136672266+khaisendev@users.noreply.github.com>
2024-02-20 13:58:54 +0500

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-  document.documentElement.style.setProperty('--scrollTop', `${document.documentElement.scrollTop}px`);
-});
-
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-ScrollSmoother.create({
-  wrapper: '.wrapper',
-  content: '.content'
-});
